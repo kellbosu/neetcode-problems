d7aa21884dde81b044e2549f385f9dc4c24ae20f
Brandon <kellb@oregonstate.edu>
2025-08-13 07:38:37 -0400

    valid_sudoku solution.

--- arrays_and_hashing/valid_sudoku.py
+++ arrays_and_hashing/valid_sudoku.py
@@ -1,2 +1,38 @@
+import collections
+
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    def isValidSudoku(self, board):
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key: (r//3, c//3)
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == ".":
+                    continue
+                if (val in rows[r] or
+                    val in cols[c] or
+                    val in squares[(r // 3, c // 3)]):
+                    return False
+                rows[r].add(val)
+                cols[c].add(val)
+                squares[(r // 3, c // 3)].add(val)
+        return True
+
+
+board = [
+    ["1","2",".",".","3",".",".",".","."],
+    ["4",".",".","5",".",".",".",".","."],
+    [".","9","8",".",".",".",".",".","3"],
+    ["5",".",".",".","6",".",".",".","4"],
+    [".",".",".","8",".","3",".",".","5"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".",".",".",".",".",".","2",".","."],
+    [".",".",".","4","1","9",".",".","8"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+
+sol = Solution()
+answer = sol.isValidSudoku(board)
+print(answer)
